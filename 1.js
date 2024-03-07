697a377f99b15eab83a43c72ea931654199a5eff
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-03-07 16:26:47 +0530

    learnt about npm and package.json

---
+++ 1.js
@@ -0,0 +1,11 @@
+console.log("Hello there I am subh ");
+
+// we can run this file by command node <filename> 
+
+// console.log(window); // this will give error 
+// why this is happening that some of the features of window are not working on node.js ?
+// this is because all the features of DOM and window objects are not included in node js
+// features like file handle and cryptography has been included its not its not just that they included what ever they wanted
+
+// whenever we wanna start a project we need to do npm init
+//  npm stands for node package manager 
